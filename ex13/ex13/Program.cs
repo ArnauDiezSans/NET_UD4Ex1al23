06a3393237d68af2f1bdfcb13710812d00e2973c
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:18:10 +0100

    ex13

---
+++ ex13/ex13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido {0}", nombre);
+        }
+    }
+}
