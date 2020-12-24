fb2cda6ec6ce0ea81c1459206ef2da7ae457ecc1
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:14:49 +0100

    ex11

---
+++ Ex11/Ex11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x;
+            int y;
+            //Console.WriteLine("Introduce el primer numero:");
+            //x = Console.ReadLine();
+            x = 3;
+            //Console.WriteLine("Introduce el segundo numero:");
+            //y = Console.ReadLine();
+            y = 5;
+            if (x > y) {
+                Console.WriteLine("El mayor es {0}", x);
+            }
+            else
+            {
+                if (y > x) 
+                {
+                    Console.WriteLine("El mayor es {0}", y);
+                }
+                else
+                {
+                    Console.WriteLine("Los numeros son iguales");
+                }
+
+            }
+            
+
+        }
+    }
+}
