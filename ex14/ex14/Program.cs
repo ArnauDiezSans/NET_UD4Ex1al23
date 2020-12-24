571ab7546f8903878d19fc8e4296014c9373952c
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:23:38 +0100

    ex14

---
+++ ex14/ex14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el radio del circulo:");
+            double radio;
+            radio = Convert.ToDouble(Console.ReadLine());
+            double resultado=Math.Pow(Math.PI, 2)*radio;
+            Console.WriteLine("Resultado:{0}", resultado);
+        }
+    }
+}
