5a4a897e4dc511477d1e484093b18dfc854c7046
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 10:07:21 +0100

    ex2

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Introduce tu nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Introduce tu ciudad");
+            string ciudad = Console.ReadLine();
+            Console.WriteLine("Te llamas {0} y vives en {1}", nombre, ciudad);
+
+        }
+    }
+}
