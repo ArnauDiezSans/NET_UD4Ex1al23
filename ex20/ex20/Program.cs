e5503b646e57eebc118baed554d811b111b9200f
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:44:06 +0100

    ex20

---
+++ ex20/ex20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el numero de ventas a realizar:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 1;
+            double total=0;
+            while (y <= x)
+            {
+                Console.WriteLine("Introduce la venta {0}", y);
+                total = total + Convert.ToDouble(Console.ReadLine());
+                y++;
+            }
+            Console.WriteLine("El total de las ventas es: {0}", total);
+        }
+    }
+}
