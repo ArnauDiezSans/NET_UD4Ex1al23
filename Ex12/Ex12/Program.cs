679ff7f2942a8f0c11dde7659b6a13611e9494fe
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:16:10 +0100

    ex12

---
+++ Ex12/Ex12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Arnau";
+            Console.WriteLine("Bienvenido {0}",nombre);
+        }
+    }
+}
