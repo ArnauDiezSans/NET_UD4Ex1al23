05307edb8bdbd50cd393b91f360a39583f89d312
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:04:13 +0100

    ex8

---
+++ ex8/ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x = 1;
+            while (x <= 100)
+            {
+                Console.WriteLine("Repetición {0}", x);
+                x++;
+            }
+        }
+    }
+}
