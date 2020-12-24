cdb99a66d5705d3439e469b260a7ed47b69d6f9f
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:30:54 +0100

    ex16

---
+++ Ex16/Ex16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el precio sin IVA");
+            double precio = Convert.ToDouble(Console.ReadLine());
+            const double IVA = 0.21;
+            Console.WriteLine("El precio con 21% de IVA es: {0}", precio + precio * IVA);
+
+        }
+    }
+}
