75e0bbf84be5d6fbfb180a6bb1e57f3b1a6a3c25
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:26:17 +0100

    ex15

---
+++ ex15/ex15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Es divisible por 2");
+            }
+            else
+            {
+                Console.WriteLine("No es divisible por 2");
+            }
+        }
+    }
+}
