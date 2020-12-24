e5a58594e5bf1dea62ab2d26f5a6a0da1bfe1b39
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:02:10 +0100

    ex7

---
+++ Ex7/Ex7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x;
+            for (x = 0; x < 100; x++)
+            {
+                Console.WriteLine("Repetición {0}", x+1);
+            }
+        }
+    }
+}
