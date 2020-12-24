efa2f7b7c05aa52d5bf6d58144e481968bbe2306
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 09:59:39 +0100

    ex1

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x = 2, y = 3, z = 4;
+            int a = x + y + z;
+            Console.WriteLine("{0},{1},{2}={3}", x, y, z, a);
+
+        }
+    }
+}
