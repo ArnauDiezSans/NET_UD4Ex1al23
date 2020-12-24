88fc79e4e657002cd88c844f09732a8f82649fd6
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:32:48 +0100

    ex17

---
+++ ex17/ex17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x <= 100)
+            {
+                Console.WriteLine("Número:{0}", x);
+                x++;
+            }
+        }
+    }
+}
