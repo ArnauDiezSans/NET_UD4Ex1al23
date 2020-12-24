6dad588ee76c761cb106da5816d292be83da696e
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:05:48 +0100

    ex9

---
+++ ex9/ex9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x = 1;
+            while (x <= 100)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine("Repetición {0}", x);
+                }
+                x++;
+            }
+        }
+    }
+}
