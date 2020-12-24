7556215b9d93d751d4986d9218280dab28ed73c7
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:39:38 +0100

    ex19

---
+++ ex19/ex19/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 1; x <= 100; x++)
+            {
+                if ((x % 2 == 0) | (x % 3 == 0))
+                {
+                    Console.WriteLine("{0}", x);
+                }
+            }
+        }
+    }
+}
