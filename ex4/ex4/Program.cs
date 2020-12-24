1fee09b477988a676aa766204ebaf71ee9df655e
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 10:14:50 +0100

    ex4

---
+++ ex4/ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x;
+            int y;
+            Console.WriteLine("Introduce el primer numero:");
+            x = Console.ReadLine();
+            Console.WriteLine("Introduce el segundo numero:");
+            y = Console.ReadLine();
+            if x > y {
+                Console.WriteLine("El mayor es {0}", x);
+            }
+            else {
+                Console.WriteLine("El mayor es {0}", y);
+            }
+        }
+    }
+}
