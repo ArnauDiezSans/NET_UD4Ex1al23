b873392ceed13d9ac08e6bd2f09c33e51f34ffd6
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 12:02:38 +0100

    ex22

---
+++ ex22/ex22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1234";
+            int okei = 0;
+            int intento = 1;
+            while (okei == 0)
+            {
+                Console.WriteLine("Introduce la contraseña. (Intento {0})", intento);
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("Enhorabuena");
+                    okei = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Error...");
+                    intento++;
+                }
+            }
+        }
+    }
+}
