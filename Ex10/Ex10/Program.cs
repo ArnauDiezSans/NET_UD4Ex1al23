dab2a7c2efae23b68ff38a6afcb945ce952409b8
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-24 11:08:31 +0100

    ex10

---
+++ Ex10/Ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x = 1;
+            while (x <= 100)
+            {
+                if ((x % 2 == 0)|(x%3==0))
+                {
+                    Console.WriteLine("Repetición {0}", x);
+                }
+                x++;
+            }
+        }
+    }
+}
